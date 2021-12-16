9fe623f5d56971a66e095ceb89b51667ac1bf1b0
Quentin Macquart <macquart.quentin@gmail.com>
2021-12-16 16:51:14 +0100

    Add Food page and FoodSlider and the CSS for it

---
+++ src/pages/Food/FoodSlider/style.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+const SFoodSlider = styled.section`
+  overflow: contain;
+  position: relative;
+  aspect-ratio: 2/3;
+  height: 220px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 40px;
+  margin: 40px 30px 10px 30px;
+
+  .imgContain {
+    overflow: hidden;
+    position: absolute;
+    bottom: 55%;
+    left: 9%;
+    width: 120px;
+    height: 120px;
+    overflow: hidden;
+    border-radius: 50%;
+  }
+  .typeImg {
+    height: 100%;
+    object-position: center;
+    object-fit: cover;
+  }
+  .typeName {
+    margin-top: 35%;
+    font-size: 20px;
+    font-weight: 600;
+    align-items: center;
+  }
+  .price {
+    margin-top: 10%;
+  }
+  .swiperComponent {
+    width: 370px;
+    background-color: yellow;
+  }
+`;
+
+export default SFoodSlider;
--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -1,3 +1,124 @@
+// import { useState } from 'react';
+import MainButton from 'components/MainButton';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import FoodSlider from './FoodSlider/index';
+import SFood from './style';
+import 'react-tabs/style/react-tabs.css';
+
 export default function Food() {
-  return <p>Food</p>;
+  // const [ModalFood, setModalFood] = useState(false);
+
+  const plats = [
+    {
+      name: 'Tartiflette',
+      price: '4€50',
+      img: 'https://files.meilleurduchef.com/mdc/photo/recette/tartiflette/tartiflette-640.jpg',
+    },
+    {
+      name: 'Tartiflette',
+      price: '4€50',
+      img: 'https://files.meilleurduchef.com/mdc/photo/recette/tartiflette/tartiflette-640.jpg',
+    },
+    {
+      name: 'Tartiflette',
+      price: '4€50',
+      img: 'https://files.meilleurduchef.com/mdc/photo/recette/tartiflette/tartiflette-640.jpg',
+    },
+    {
+      name: 'Tartiflette',
+      price: '4€50',
+      img: 'https://files.meilleurduchef.com/mdc/photo/recette/tartiflette/tartiflette-640.jpg',
+    },
+    {
+      name: 'Tartiflette',
+      price: '4€50',
+      img: 'https://files.meilleurduchef.com/mdc/photo/recette/tartiflette/tartiflette-640.jpg',
+    },
+  ];
+
+  const desserts = [
+    {
+      name: 'Tiramisu',
+      price: '5€',
+      img: 'https://media.houra.fr/images/widget/recette/gd_recette_tiramisu.jpg',
+    },
+    {
+      name: 'Tiramisu',
+      price: '5€',
+      img: 'https://media.houra.fr/images/widget/recette/gd_recette_tiramisu.jpg',
+    },
+    {
+      name: 'Tiramisu',
+      price: '5€',
+      img: 'https://media.houra.fr/images/widget/recette/gd_recette_tiramisu.jpg',
+    },
+    {
+      name: 'Tiramisu',
+      price: '5€',
+      img: 'https://media.houra.fr/images/widget/recette/gd_recette_tiramisu.jpg',
+    },
+    {
+      name: 'Tiramisu',
+      price: '5€',
+      img: 'https://media.houra.fr/images/widget/recette/gd_recette_tiramisu.jpg',
+    },
+  ];
+
+  const drinks = [
+    {
+      name: 'Coca zero',
+      price: '2€50',
+      img: 'https://www.lsa-conso.fr/mediatheque/7/4/4/000189447_5.jpg',
+    },
+    {
+      name: 'Coca zero',
+      price: '2€50',
+      img: 'https://www.lsa-conso.fr/mediatheque/7/4/4/000189447_5.jpg',
+    },
+    {
+      name: 'Coca zero',
+      price: '2€50',
+      img: 'https://www.lsa-conso.fr/mediatheque/7/4/4/000189447_5.jpg',
+    },
+    {
+      name: 'Coca zero',
+      price: '2€50',
+      img: 'https://www.lsa-conso.fr/mediatheque/7/4/4/000189447_5.jpg',
+    },
+    {
+      name: 'Coca zero',
+      price: '2€50',
+      img: 'https://www.lsa-conso.fr/mediatheque/7/4/4/000189447_5.jpg',
+    },
+  ];
+
+  return (
+    <SFood>
+      <h1 className="menuOfDay"> Le menu du jour</h1>
+      <section className="choiceMenu">
+        <Tabs>
+          <TabList>
+            <Tab>Plats</Tab>
+            <Tab>Desserts</Tab>
+            <Tab>Boissons</Tab>
+          </TabList>
+
+          <TabPanel>
+            {' '}
+            <FoodSlider typeOfFood={plats} />
+          </TabPanel>
+          <TabPanel>
+            {' '}
+            <FoodSlider typeOfFood={desserts} />
+          </TabPanel>
+          <TabPanel>
+            {' '}
+            <FoodSlider typeOfFood={drinks} />
+          </TabPanel>
+        </Tabs>
+      </section>
+
+      <MainButton content="Réserver" />
+    </SFood>
+  );
 }
