81992fbecd75f547dd27f3ad1952b2d184a177c9
BBriset <briset.b@gmail.com>
2021-12-16 16:35:46 +0100

    Build Header, links from, and styling it

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,27 @@
-export default function Calendar() {
-  return <p>Calendar</p>;
+import PropTypes from 'prop-types';
+import UserProfile from 'components/UserProfile';
+import { Link } from 'react-router-dom';
+import SHeader from './style';
+import home from '../../assets/Img/home.svg';
+
+export default function Header({ logo }) {
+  return (
+    <SHeader>
+      <Link to="/home">
+        <img src={home} alt="ico" />
+      </Link>
+      <img className="logo" src={logo} alt="logo" />
+      <Link to="/profilpage">
+        <UserProfile />
+      </Link>
+    </SHeader>
+  );
 }
+
+Header.propTypes = {
+  logo: PropTypes.string,
+};
+
+Header.defaultProps = {
+  logo: '',
+};
