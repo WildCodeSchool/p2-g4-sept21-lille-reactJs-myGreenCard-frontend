29520e2c5623facc981a0cc2e96d913c10c526d5
Quentin Macquart <macquart.quentin@gmail.com>
2021-12-16 16:49:49 +0100

    Add changes to Global style

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-
+h1,h2,h3,p,span{
+  font-family: 'Montserrat', sans-serif;
+}
 `;
 
 export default GlobalStyle;
