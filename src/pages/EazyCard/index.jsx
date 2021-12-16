b460d00003b0174bb68db432e6c5e01762612cd1
BBriset <briset.b@gmail.com>
2021-12-16 16:41:44 +0100

    Eazycard page with nfc animation

--- src/pages/EazyCard/index.jsx
+++ src/pages/EazyCard/index.jsx
@@ -1,3 +1,21 @@
+import Header from 'components/Header';
+import SEazyCard from './style';
+import nfc from '../../assets/Img/iphone.svg';
+import logo from '../../assets/Img/easyApp.png';
+import card from '../../assets/Img/easyCard.png';
+
 export default function EazyCard() {
-  return <p>EazyCard</p>;
+  return (
+    <>
+      <Header logo={logo} />
+
+      <SEazyCard>
+        <img src={card} alt="Eazycard" />
+        <div className="anime">
+          <img className="nfc" src={nfc} alt="NFC" />
+        </div>
+        <p> Approchez du lecteur</p>
+      </SEazyCard>
+    </>
+  );
 }
