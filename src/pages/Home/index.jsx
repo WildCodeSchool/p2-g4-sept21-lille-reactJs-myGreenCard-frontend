80df27304c4bea0c5630e51ee8bd940601aae013
clairedelaleau <claire.delaleau@gmail.com>
2021-12-22 09:42:54 +0100

    pre-merge

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,113 @@
+import { Link } from 'react-router-dom';
+import Salade from './pictures/salad.png';
+import Boeuf from './pictures/beef.png';
+import Pizza from './pictures/pizza.jpeg';
+import Muffin from './pictures/muffin.jpeg';
+import Office from './pictures/office.png';
+import MeetingRoom from './pictures/meetingRoom.png';
+import Furnitures from './pictures/furnitures.png';
+import Card from './pictures/eazyCard.jpeg';
+import SHome from './style';
+
+const meals = [
+  {
+    picture: Salade,
+    name: 'Salade',
+  },
+  {
+    picture: Boeuf,
+    name: 'Boeuf',
+  },
+  {
+    picture: Pizza,
+    name: 'Pizza',
+  },
+  {
+    picture: Muffin,
+    name: 'Muffin',
+  },
+];
+
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <SHome>
+      <Link to="./Food">
+        <article className="menu">
+          <div className="meal">
+            <h2>Le menu du jour</h2>
+          </div>
+          <div className="mealList">
+            <div className="description">
+              <li>Salade tomates et avocats</li>
+              <li>Rôti de Boeuf</li>
+              <li>Pizza aux trois légumes</li>
+              <li>Muffin aux pépites de chocolat</li>
+            </div>
+            <div className="dishes">
+              {meals.map((meal) => {
+                return <img src={meal.picture} alt={meal.name} />;
+              })}
+            </div>
+          </div>
+          <div className="dinnersReady">
+            <p>A Table !</p>
+          </div>
+        </article>
+      </Link>
+      <Link to="./Office">
+        <article className="office">
+          <div className="reservation">
+            <h2>Réservation de bureau</h2>
+          </div>
+          <div className="chooseYourOffice">
+            <p>Choisissez votre bureau</p>
+          </div>
+          <div className="officePicture">
+            <img src={Office} alt={Office} />
+          </div>
+        </article>
+      </Link>
+      <Link to="./Meeting">
+        <article className="meeting">
+          <div className="roomReservation">
+            <h2>Réservation de salle de réunion</h2>
+          </div>
+
+          <div className="meetingReservation">
+            <p>Réservez votre salle de réunion</p>
+          </div>
+          <div className="meetingRoomPicture">
+            <img src={MeetingRoom} alt={MeetingRoom} />
+          </div>
+        </article>
+      </Link>
+      <Link to="./Supplies">
+        <article className="furnitures">
+          <div className="commande">
+            <h2>Commande de fournitures</h2>
+          </div>
+
+          <div className="shop">
+            <p>Ajoutez les articles dont vous avez besoin</p>
+          </div>
+          <div className="furniture">
+            <img src={Furnitures} alt={Furnitures} />
+          </div>
+        </article>
+      </Link>
+      <Link to="./LostCard">
+        <article className="lostcard">
+          <div className="card">
+            <h2>Carte perdue</h2>
+          </div>
+          <div className="unactivCard">
+            <p>Désactiver votre carte en cas de perte</p>
+          </div>
+          <div className="pictureCard">
+            <img src={Card} alt={Card} />
+          </div>
+        </article>
+      </Link>
+    </SHome>
+  );
 }
--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -1,5 +1,180 @@
 import styled from 'styled-components';
 
-const SHome = styled.main``;
+const SHome = styled.section`
+
+margin: 1.75rem;
+
+article {
+    height: 200px;
+    width: 80vw;
+    margin: 20px 0 10px 0px;
+    border-radius: 10px;
+    -webkit-box-shadow: 0px 7px 12px -3px #000000;
+    box-shadow: 0px 7px 12px -3px #000000;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .menu {
+    height: 180px;
+    font-weight: bold;
+  }
+  .meal {
+    height: 30px;
+    width: 80vw;
+  }
+  .mealList {
+    font-weight: normal;
+    display: flex;
+    flex-flow: raw;
+    height: 100px;
+  }
+  .description {
+    list-style: none;
+    width: 60vw;
+    text-align: left;
+  }
+ 
+  .dishes {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: right;
+    height: 80px;
+    width: 50px;
+    justify-content: space-between;
+    img {
+      border-radius: 45px;
+      object-fit: cover;
+      height: 35px;
+      width: 20px;
+    }
+  
+  }
+    .dinnersReady{
+    height: 30px;
+    text-decoration: underline;
+  }
+
+
+
+  //
+
+  .office {  
+    display: flex;
+    flex-wrap: wrap;
+    font-weight: bold;
+    height:150px;
+  }
+  .reservation {
+    height: 50px;
+    width: 40vw;
+  }
+  /*.officeReservation {
+    font-weight: normal;
+    display: flex;
+    flex-flow: raw;
+    height: 170px;
+  }*/
+  }
+  .chooseYourOffice {
+    font-weight: normal;
+    height: 100px;
+    width:30vw;
+
+  }
+
+  .officePicture {
+    height: 150px;
+    width: 45vw;
+
+    img {
+      height: 150px;
+      width: 40vw;
+      border-radius: 15px;
+     
+    }
+  }
+
+//
+.meeting {
+  display: flex;
+    flex-wrap: wrap;
+    font-weight: bold;
+    height:150px;
+}
+.roomReservation {
+  height: 50px;
+    width: 40vw;
+}
+.meetingReservation {
+  font-weight: normal;
+    height: 100px;
+    width:30vw;
+
+}
+  .meetingRoomPicture {
+   
+    height: 150px;
+    width:40vw;
+    
+    img {
+      height: 150px;
+      width: 40vw;
+      
+    }
+  }
+//
+.furnitures {
+  display: flex;
+  flex-wrap: wrap;
+  font-weight: bold;
+  height: 150px;
+}
+.commande {
+  height: 50px;
+  width: 40vw;
+}
+.shop {
+  font-weight: normal;
+    height: 100px;
+    width:30vw;
+}
+  .furniture 
+    height: 140px;
+    width: 40vw;
+    img {
+      height: 140px;
+      width: 40vw;
+    }
+  }
+//
+.lostcard {
+  display: flex;
+  flex-wrap: wrap;
+  font-weight: bold;
+  height: 150px;
+
+}
+.card {
+  height: 40px;
+  width: 40vw;
+}
+.unactivCard { 
+   font-weight: normal;
+    height: 110px;
+    width:30vw;}
+
+.pictureCard  {
+    
+    height: 80px;
+    width: 40px;
+    img {
+      object-fit: contain;
+     
+    }
+
+
+`;
 
 export default SHome;
