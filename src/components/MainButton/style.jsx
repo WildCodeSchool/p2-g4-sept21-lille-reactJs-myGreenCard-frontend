5eb50dcb405cc67741a7228a55877d92515c71d6
Quentin Macquart <macquart.quentin@gmail.com>
2021-12-22 16:12:51 +0100

    Change style of the component to looklike the wireframe

--- src/components/MainButton/style.jsx
+++ src/components/MainButton/style.jsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const SMainButton = styled.button`
-  background-color: black;
-  color: white;
+  box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+    rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+    rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  background-color: white;
+  color: black;
   font-family: 'Montserrat', sans-serif;
-  font-weight: 600;
+  font-weight: 400;
   letter-spacing: 1px;
   border: none;
   border-radius: 10px;
