a5d2b775d4a3f4d06787df10da341ff62c73106c
Quentin Macquart <macquart.quentin@gmail.com>
2021-12-22 16:13:45 +0100

    Add updates on the modal to transfer data from slider into it and style it

--- src/pages/Food/FoodModal/index.jsx
+++ src/pages/Food/FoodModal/index.jsx
@@ -1,32 +1,64 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import MainButton from 'components/MainButton';
 import SFoodModal from './style';
 
-function FoodModal() {
+function FoodModal({ menuElement }) {
   const [renderModal, setRenderModal] = useState(true);
 
   const hideTheModal = () => {
     setRenderModal(!renderModal);
   };
-
   return (
     <>
       {renderModal && (
         <SFoodModal>
-          <div>
-            <h2>Choisir ce plat </h2>
+          <section>
+            <h2>Votre choix </h2>
+            <article>
+              <div className="imgContain">
+                <img src={menuElement.picture} alt="" />
+              </div>
+              <p className="foodTitle">{menuElement.name}</p>
+              <span>{menuElement.price}</span>
+            </article>
+          </section>
+          <span>{menuElement.calories}</span>
+          <p> Contient:</p>
+          <section className="foodContent">
+            {menuElement.content.map((oneContent) => (
+              <p className="FoodContent">{oneContent},&#160; </p>
+            ))}
+          </section>
+          <div className="buttonWrapper">
+            <MainButton
+              clickCallback={() => {
+                hideTheModal();
+              }}
+              content="Changer"
+            />
+            <MainButton content="Valider" />
           </div>
-          <MainButton content="Valider" />
-          <MainButton
-            clickCallback={() => {
-              hideTheModal();
-            }}
-            content="Changer"
-          />
         </SFoodModal>
       )}
     </>
   );
 }
+
+FoodModal.propTypes = {
+  menuElement: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    price: PropTypes.string,
+    calories: PropTypes.string,
+    picture: PropTypes.string,
+    content: PropTypes.arrayOf(PropTypes.string),
+    allergies: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
+FoodModal.defaultProps = {
+  menuElement: null,
+};
 
 export default FoodModal;
--- src/pages/Food/FoodModal/style.jsx
+++ src/pages/Food/FoodModal/style.jsx
@@ -4,11 +4,66 @@ const SFoodModal = styled.section`
   text-align: center;
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 100vh;
   background-color: #eeeded;
   position: absolute;
   width: 100vw;
   z-index: 2;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  article {
+    border-radius: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    background-color: white;
+    width: 230px;
+    height: 300px;
+    margin: 50px 0 20px 0;
+  }
+  img {
+    height: 100%;
+    object-position: center;
+    object-fit: cover;
+  }
+  .imgContain {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+      rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    overflow: hidden;
+    position: absolute;
+    top: 5%;
+    left: 26%;
+    width: 180px;
+    height: 180px;
+    overflow: hidden;
+    border-radius: 50%;
+  }
+  .foodTitle {
+    width: 60%;
+    margin-bottom: 10%;
+    font-size: 23px;
+    font-weight: 600;
+    align-items: center;
+  }
+  span {
+    margin-bottom: 10px;
+  }
+  .foodContent {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 30px;
+  }
+  .buttonWrapper {
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+  }
 `;
 
 export default SFoodModal;
