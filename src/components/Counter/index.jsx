f7c6f04970db111ee5edf0ec33fb4a919a7fa992
Othmane Khiter <othmane.khiter@gmail.com>
2022-01-10 16:30:48 +0100

    Updated the counter to lock it at 0 when we want to decrease under 0 (and don't have negatives values)

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -3,20 +3,17 @@ import SCounter from './style';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const prev = () => (count <= 0 ? 0 : setCount((prevCount) => prevCount - 1));
+  const next = () => setCount((nextCount) => nextCount + 1);
 
   return (
     <SCounter>
-      <button
-        type="button"
-        onClick={() => setCount((prevCount) => prevCount - 1)}
-      >
+      <button type="button" onClick={prev}>
         -
       </button>
+
       <h3>{count}</h3>
-      <button
-        type="button"
-        onClick={() => setCount((prevCount) => prevCount + 1)}
-      >
+      <button type="button" onClick={next}>
         +
       </button>
     </SCounter>
