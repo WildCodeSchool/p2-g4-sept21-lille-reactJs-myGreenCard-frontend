4a5ac2247e8d51f72e3d27ecc3fcee9006046dc7
Quentin Macquart <macquart.quentin@gmail.com>
2022-01-12 14:31:59 +0100

    Add GlobalStyle and conditions in the file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,34 +3,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ToggleButton from '../../components/ToggleButton';
 import logo from '../../assets/Img/easyApp.png';
-import Salade from './pictures/salad.png';
-import Boeuf from './pictures/beef.png';
-import Pizza from './pictures/pizza.jpeg';
-import Muffin from './pictures/muffin.jpeg';
-import Office from './pictures/office.png';
-import MeetingRoom from './pictures/meetingRoom.png';
-import Furnitures from './pictures/furnitures.png';
+import darkLogo from '../../assets/Img/easyAppNight.png';
+import food from './pictures/food.png';
+import darkFood from './pictures/darkFood.png';
+import office from './pictures/office.png';
+import darkOffice from './pictures/darkOffice.png';
+import meetingRoom from './pictures/meetingRoom.png';
+import darkMeeting from './pictures/darkMeeting.png';
+import furnitures from './pictures/furnitures.png';
+import darkSupplies from './pictures/darkSupplies.png';
 import Card from './pictures/eazyCard.jpeg';
 import SHome from './style';
-
-const meals = [
-  {
-    picture: Salade,
-    name: 'Salade',
-  },
-  {
-    picture: Boeuf,
-    name: 'Boeuf',
-  },
-  {
-    picture: Pizza,
-    name: 'Pizza',
-  },
-  {
-    picture: Muffin,
-    name: 'Muffin',
-  },
-];
 
 export default function Home({ theme, setTheme }) {
   const isDarkTheme = theme === 'dark';
@@ -39,18 +22,20 @@ export default function Home({ theme, setTheme }) {
   };
   return (
     <>
-      <Header logo={logo} />
+      <Header logo={isDarkTheme ? darkLogo : logo} />
       <SHome>
         <ToggleButton
           label="Dark/light mode"
           handleClick={toggleTheme}
           className={isDarkTheme ? 'dark' : null}
-          size={0.75}
+          size={0.5}
         />
         <Link to="/food">
-          <article className="menu">
+          <article
+            className={isDarkTheme ? 'darkThemeBackground menu' : 'menu'}
+          >
             <div className="meal">
-              <h2>Le menu du jour</h2>
+              <h3>Le menu du jour</h3>
             </div>
             <div className="mealList">
               <div className="description">
@@ -59,60 +44,77 @@ export default function Home({ theme, setTheme }) {
                 <p>Pizza aux légumes</p>
                 <p>Muffin</p>
               </div>
-              <div className="dishes">
-                {meals.map((meal) => {
-                  return <img src={meal.picture} alt={meal.name} />;
-                })}
+              <div className="foodIcon">
+                <img
+                  src={isDarkTheme ? darkFood : food}
+                  alt="Icone menu du jour"
+                />
               </div>
             </div>
             <div className="dinnersReady">
-              <p>A Table !</p>
+              <p>A table !</p>
             </div>
           </article>
         </Link>
         <Link to="/office">
-          <article className="office">
+          <article
+            className={isDarkTheme ? 'darkThemeBackground office' : 'office'}
+          >
             <div className="reservation">
-              <h2>Réservation de bureau</h2>
+              <h3>Réservation de bureau</h3>
             </div>
             <div className="chooseYourOffice">
               <p>Choisissez votre bureau</p>
             </div>
             <div className="officePicture">
-              <img src={Office} alt="Bureau" />
+              <img src={isDarkTheme ? darkOffice : office} alt="Bureau" />
             </div>
           </article>
         </Link>
         <Link to="/meetingRooms">
-          <article className="meeting">
-            <div className="roomReservation">
-              <h2>Réservation de salle de réunion</h2>
-            </div>
-            <div className="meetingReservation">
+          <article
+            className={isDarkTheme ? 'darkThemeBackground meeting' : 'meeting'}
+          >
+            <div className="meetingReservation ">
+              <h3>Réservation de salle de réunion</h3>
               <p>Réservez votre salle de réunion</p>
             </div>
             <div className="meetingRoomPicture">
-              <img src={MeetingRoom} alt="Salle de réunion" />
+              <img
+                src={isDarkTheme ? darkMeeting : meetingRoom}
+                alt="Salle de réunion"
+              />
             </div>
           </article>
         </Link>
         <Link to="/supplies">
-          <article className="furnitures">
+          <article
+            className={
+              isDarkTheme ? 'darkThemeBackground furnitures' : 'furnitures'
+            }
+          >
             <div className="order">
-              <h2>Commande de fournitures</h2>
+              <h3>Commande de fournitures</h3>
             </div>
             <div className="shop">
               <p>Ajoutez les articles dont vous avez besoin</p>
             </div>
             <div className="pictureFurniture">
-              <img src={Furnitures} alt="Fournitures" />
+              <img
+                src={isDarkTheme ? darkSupplies : furnitures}
+                alt="Fournitures"
+              />
             </div>
           </article>
         </Link>
         <Link to="/lostCard">
-          <article className="lostCard">
+          <article
+            className={
+              isDarkTheme ? 'darkThemeBackground lostCard' : 'lostCard'
+            }
+          >
             <div className="card">
-              <h2>Carte perdue</h2>
+              <h3>Carte perdue</h3>
             </div>
             <div className="unactivCard">
               <p>Désactiver votre carte en cas de perte</p>
