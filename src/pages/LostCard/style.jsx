ebd99d1e690076b73eac94bd2061c954359945a1
BBriset <briset.b@gmail.com>
2022-01-14 16:15:02 +0100

    LostCard pages building

--- src/pages/LostCard/style.jsx
+++ src/pages/LostCard/style.jsx
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
 
-const SLostCard = styled.main``;
+const SLostCard = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 100vh;
+
+  main {
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 70vh;
+    m h2 {
+      font-size: 28px;
+      font-weight: bolder;
+    }
+    .job {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      width: 90vw;
+      height: 7vh;
+    }
+    .disable {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      width: 90vw;
+      font-size: 22px;
+      font-weight: bold;
+    }
+  }
+  .modal {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
 
 export default SLostCard;
