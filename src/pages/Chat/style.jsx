3627a89cf9e4f135a130fb5dbf87bb15e6acf9ea
HeloiseFerla <ferla.heloise@gmail.com>
2022-01-25 16:25:24 +0100

    Add a chat

---
+++ src/pages/Chat/style.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const SChat = styled.main`
+  height: 100vh;
+  ul {
+  }
+`;
+export default SChat;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -156,6 +156,13 @@ export default function Home({ theme, setTheme }) {
             </div>
           </article>
         </Link>
+        <Link to="/chat">
+          <article
+            className={isDarkTheme ? 'darkThemeBackground chat' : 'chat'}
+          >
+            <h3>Chat</h3>
+          </article>
+        </Link>
       </SHome>
     </>
   );
