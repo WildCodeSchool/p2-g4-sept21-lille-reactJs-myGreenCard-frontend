653fc2717f412389076a4a6910af8a91e2c6b339
BBriset <briset.b@gmail.com>
2022-02-07 17:35:18 +0100

    fix chat

--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -174,14 +174,16 @@ const SHome = styled.section`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-around;
     div {
       display: flex;
       flex-direction: column;
-    }
-    h3 {
-      font-weight: bold;
-      margin: 10px;
+      width: 40vw;
+      h3 {
+        font-weight: bold;
+      }
+      p {
+        margin-top: 10px;
+      }
     }
   }
 
