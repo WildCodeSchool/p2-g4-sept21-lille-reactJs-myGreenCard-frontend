3e707fa623e0ba2008077cdb510eb156aa7ad83c
BBriset <briset.b@gmail.com>
2022-02-07 11:35:45 +0100

    Fix avatar dark theme

--- src/components/UserAvatar/index.jsx
+++ src/components/UserAvatar/index.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import logo from 'assets/Img/Harfang.png';
+import logoDark from 'assets/Img/HarfangDark.png';
 import SUserAvatar from './style';
 
 export default function UserAvatar({ size, border }) {
   const user = useSelector((state) => state.user);
+  const themeStorage = localStorage.getItem('theme');
   return (
     <SUserAvatar width={size} height={size} border={border}>
-      <img
-        src={user.id ? user.picture : logo}
-        alt={user.id ? user.name : 'EazyApp'}
-      />
+      {themeStorage === 'light' ? (
+        <img
+          src={user.id ? user.picture : logo}
+          alt={user.id ? user.name : 'EazyApp'}
+        />
+      ) : (
+        <img
+          src={user.id ? user.picture : logoDark}
+          alt={user.id ? user.name : 'EazyApp'}
+        />
+      )}
     </SUserAvatar>
   );
 }
